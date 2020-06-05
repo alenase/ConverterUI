212ab25961bda910fa4dffb096ac0d431bfeb911
Evgeniy <evgeniy.koren90@gmail.com>
2020-06-05 04:00:08 +0300

    added components: AboutApi, TableInfo

--- src/components/AboutAPI.js
+++ src/components/AboutAPI.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import '../App.css';
+import TableInfo from './TableInfo';
+import Container from 'react-bootstrap/Container';
 
 function AboutAPI() {
     return (
-      <div >
-      </div>
+      <Container id="Body">
+        <h1>Privat Api</h1>
+        <p>Наш сайт позволяет производить конвертацию между следующими валютами</p>
+        <h4 align = "left">Cписок доступных валют:</h4>
+        <TableInfo />
+      </Container>
     );
   }
   
-  export default AboutAPI;
+export default AboutAPI;
   
